0416612e39757dbf998a4ee14e926b22a447ff9e
Sergio Rivera <sriveralopez50@gmail.com>
2023-03-05 18:45:13 +0000

    Created script to build and push Docker images

---
+++ scripts/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Configuration variables
+DOCKERHUB_ACCOUNT=sergiorivera50
+
+# Set the services directory for the project
+BASE_DIR=$(cd "$(dirname "$0")" && cd .. && pwd)
+SERVICES_DIR="$BASE_DIR/services"
+
+# Build and tag all Docker images
+docker build -t $DOCKERHUB_ACCOUNT/sigma-auth:latest $SERVICES_DIR/auth
+docker build -t $DOCKERHUB_ACCOUNT/sigma-registry:latest $SERVICES_DIR/registry
+
+# Push all Docker images
+docker push $DOCKERHUB_ACCOUNT/sigma-auth:latest
+docker push $DOCKERHUB_ACCOUNT/sigma-registry:latest
