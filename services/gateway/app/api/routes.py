d7ff3af55cae8f4816a500d6f8f43d87610bbb1e
Sergio Rivera <sriveralopez50@gmail.com>
2023-03-07 09:23:03 +0000

    Protect gateway routes with authentication

--- services/gateway/app/api/routes.py
+++ services/gateway/app/api/routes.py
@@ -1,28 +1,20 @@
 from flask import Blueprint, request, current_app
 import requests
-from requests.auth import HTTPBasicAuth
+from gateway.app.util.decorators import authorized
 
 bp = Blueprint("auth", __name__)
 
 
 @bp.route("/deploy", methods=["POST"])
+@authorized
 def deploy():
-    if not request.headers.get("Authorization"):
-        return "Did not send credentials", 401
-
-    auth_host = current_app.config.get("AUTH_SVC_HOST")
-    login_response = requests.post(f"{auth_host}/login",
-                                   auth=HTTPBasicAuth(request.authorization.username,
-                                                      request.authorization.password))
-    if login_response.status_code != 200:
-        return "Unauthorized", 401
-
     registry_host = current_app.config.get("REGISTRY_SVC_HOST")
     register_response = requests.post(f"{registry_host}/register", data=request.form, files=request.files)
     return register_response.text, 200
 
 
 @bp.route("/<model_id>/predict", methods=["POST"])
+@authorized
 def predict(model_id):
     dev_host = current_app.config.get("INFERENCE_SVC_HOST")
     prod_host = f"http://{model_id}.default.svc.cluster.local:3000"
