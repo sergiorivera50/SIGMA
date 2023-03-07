b3364ab899f46255f0ef460b40918cb4232cb9f7
Sergio Rivera <sriveralopez50@gmail.com>
2023-03-07 09:56:01 +0000

    Get auth service via app configuration instead of env var

--- services/gateway/app/util/decorators.py
+++ services/gateway/app/util/decorators.py
@@ -1,5 +1,5 @@
 import functools
-from flask import request
+from flask import request, current_app
 import requests
 import os
 
@@ -7,7 +7,7 @@ import os
 def authorized(f):
     @functools.wraps(f)
     def decorated_function(*args, **kwargs):
-        auth_host = os.environ.get("AUTH_SVC_HOST")
+        auth_host = current_app.config.get("AUTH_SVC_HOST")
         response = requests.post(f"{auth_host}/validate", headers=request.headers)
         if response.status_code != 200:
             return response.content, response.status_code, response.headers.items()
